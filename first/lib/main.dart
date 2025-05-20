76f63cfe65eb95798f7abe2f8b04bbc133c7c090
f25ba048 <840AB52F!dic>
2025-05-20 16:02:58 +0900

    first commit

---
+++ first/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter!'),
+        ),
+        body: Text(
+          'Hello, Flutter World!!',
+         style: TextStyle(fontSize:32.0),
+      ),
+    ),
+);
+  }
+}
